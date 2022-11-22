e8fe84db5a7e509d6846a2f127e7e3fe0a156df9
Charlie <chickchick.ce@gmail.com>
2022-11-22 10:57:23 +0700

    initial

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Goal
+//
+//  Created by Charlie Hoang on 11/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
